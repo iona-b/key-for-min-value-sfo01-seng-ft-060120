506d557da7694ec9520404bcb9b574132c2b21df
Iona <ionabrabender@gmail.com>
2020-05-20 19:27:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if min_value == nil
         min_value = value
+        key_w_min_value = key
       elsif value < min_value
         min_value = value
         key_w_min_value = key
